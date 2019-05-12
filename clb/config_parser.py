ef0141705848668f4820a9fbc8aaf097a795f1ca
Andrey Okulov <okulov@ya.ru>
2019-05-12 22:00:31 +0300

    [ref] config_parser

--- clb/config_parser.py
+++ clb/config_parser.py
@@ -10,30 +10,34 @@ from clb.logger import init_logger
 
 LOG = init_logger(__name__, 'DEBUG')
 
-PARSER = argparse.ArgumentParser()
-PARSER.add_argument(
-    '-c', '--config',
-    required=True,
-    type=argparse.FileType('r'),
-    help='path to config file'
-)
-ARGS = PARSER.parse_args()
 
-try:
-    LOG.info(f'Reading config file: {ARGS.config.name}')
-    with open(ARGS.config.name, 'r') as yaml_file:
-        CONFIG = yaml.safe_load(yaml_file)
-except (
-        FileNotFoundError,
-        yaml.YAMLError,
-)as error:
-    LOG.critical({error})
-    sys.exit(1)
+def get_config_path() -> str:
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '-c', '--config',
+        required=True,
+        type=argparse.FileType('r'),
+        help='path to config file'
+    )
+    return parser.parse_args().config.name
+
+
+def get_config(config_path: str) -> dict:
+    try:
+        LOG.info(f'Reading config file: {config_path}')
+        with open(config_path, 'r') as yaml_file:
+            return yaml.safe_load(yaml_file)
+    except (
+            FileNotFoundError,
+            yaml.YAMLError,
+    )as error:
+        LOG.critical({error})
+        sys.exit(1)
 
 
 def get_log_level() -> str:
     try:
-        log_level = CONFIG['log_level']
+        log_level = get_config(get_config_path())['log_level']
     except KeyError as error:
         LOG.warning(f'Bad config file: Key {error} not found. Setting log level to DEBUG')
         log_level = 'DEBUG'
@@ -43,7 +47,7 @@ def get_log_level() -> str:
 
 def get_config_value(key: str) -> Union[list, str, dict]:
     try:
-        return CONFIG[key]
+        return get_config(get_config_path())[key]
     except KeyError as error:
         LOG.error(f'Bad config file: Key {error} not found')
         sys.exit(1)
