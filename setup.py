50f3da1cd0b490697b2794fdf24704396bfb1939
Andrey Okulov <okulov@ya.ru>
2019-05-25 20:44:03 +0300

    [ops] Bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name='clb',
-    version='0.0.5',
+    version='0.0.6',
     author='Andrey Okulov',
     author_email='okulov@ya.ru',
     description='Command line batteries (clb)'
